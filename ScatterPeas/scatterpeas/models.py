5138f0419fc501913d467e82fac6e57d5f172aeb
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-13 13:32:50 -0700

    initial go at user model

--- ScatterPeas/scatterpeas/models.py
+++ ScatterPeas/scatterpeas/models.py
@@ -1,8 +1,12 @@
+import os
+import sqlalchemy as sa
 from sqlalchemy import (
     Column,
     Index,
     Integer,
     Text,
+    Unicode,
+    UnicodeText,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,14 +18,32 @@ from sqlalchemy.orm import (
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
+DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    'postgresql:///scatterPeas1'
+)
+
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first = Column(Unicode(50))
+    last = Column(Unicode(50))
+    username = Column(Unicode(50), nullable=False)
+    password = Column(Unicode(50), nullable=False)
+    dflt_medium = Column(Unicode(5), default='email')
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+    def __repr__(self):
+        return "<User(first=' %s', last=' %s', username=' %s', dflt_medium=' %s')>" % (self.first, self.last, self.username, self.dflt_medium)
+
+
+
+
+
+def init_db():
+    engine = sa.create_engine(DATABASE_URL, echo=True)
+    Base.metadata.create_all(engine)
+    DBSession.configure(bind=engine)
