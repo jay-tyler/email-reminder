1583c5605ec4aea79cce39d7d67e94c1644f5282
Saki Fu <sakifu.86@gmail.com>
2015-07-13 12:33:37 -0700

    created function

--- email_sender_gmail.py
+++ email_sender_gmail.py
@@ -4,44 +4,48 @@ from email.mime.text import MIMEText
 from email.MIMEImage import MIMEImage
 
 
-fromaddr = 'scatterpeas@gmail.com'
-toaddrs = 'saki.fu86@gmail.com'
+def main(fromaddr, toaddrs, subject, text, html, img):
+    msg = MIMEMultipart('mixed')
+    # msg = MIMEText("I'm scattered")
+    msg['Subject'] = subject
+    msg['From'] = fromaddr
+    msg['To'] = toaddrs
 
-msg = MIMEMultipart('mixed')
-# msg = MIMEText("I'm scattered")
-msg['Subject'] = "Greeting"
-msg['From'] = fromaddr
-msg['To'] = toaddrs
+    fp = open(img, 'rb')
+    part3 = MIMEImage(fp.read())
+    fp.close()
 
-text = "This is text \n Hi!\nHow are you?\nI'm a scattered pea.\nhttps://www.youtube.com/watch?v=jHm0jmg-sbc"
-html = """
-<html>
-  <head></head>
-  <body>
-    <p>Hi!<br>
-        This is HTML<br>
-        How are you?<br>
-        I'm a scattered pea. <a href="https://www.youtube.com/watch?v=jHm0jmg-sbc">Watch this</a>
-    </p>
-  </body>
-</html>
- """
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
 
-fp = open('nmr.jpg', 'rb')
-msgImage = MIMEImage(fp.read())
-fp.close()
+    msg.attach(part1)
+    msg.attach(part2)
+    msg.attach(part3)
 
-part1 = MIMEText(text, 'plain')
-part2 = MIMEText(html, 'html')
+    username = 'scatterpeas@gmail.com'
+    password = 'peapassword'
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(fromaddr, toaddrs, msg.as_string())
+    server.quit()
 
-msg.attach(part1)
-msg.attach(part2)
-msg.attach(msgImage)
+if __name__ == '__main__':
+    fromaddr = 'scatterpeas@gmail.com'
+    toaddrs = 'saki.fu86@gmail.com'
+    text = "This is text \n Hi!\nHow are you?\nI'm a scattered pea.\nhttps://www.youtube.com/watch?v=jHm0jmg-sbc"
+    html = """
+    <html>
+      <head></head>
+      <body>
+        <p>Hi!<br>
+            This is HTML<br>
+            How are you?<br>
+            I'm a scattered pea. <a href="https://www.youtube.com/watch?v=jHm0jmg-sbc">Watch this</a>
+        </p>
+      </body>
+    </html>
+     """
+    img = 'nmr.jpg'
+    main(fromaddr, toaddrs, 'title', text, html, img)
 
-username = 'scatterpeas@gmail.com'
-password = 'peapassword'
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.starttls()
-server.login(username, password)
-server.sendmail(fromaddr, toaddrs, msg.as_string())
-server.quit()
