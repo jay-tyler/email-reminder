7c0100e5e7e69b81b432100a18cc405b3c99717b
Saki Fu <sakifu.86@gmail.com>
2015-07-13 11:18:42 -0700

    added html feature

--- email_sender_gmail.py
+++ email_sender_gmail.py
@@ -5,10 +5,30 @@ from email.mime.text import MIMEText
 fromaddr = 'scatterpeas@gmail.com'
 toaddrs = 'saki.fu86@gmail.com'
 
-msg = MIMEText("I'm scattered")
+msg = MIMEMultipart('alternative')
+# msg = MIMEText("I'm scattered")
 msg['Subject'] = "Greeting"
 msg['From'] = fromaddr
 msg['To'] = toaddrs
+
+text = "This is text \n Hi!\nHow are you?\nI'm a scattered pea.\nhttps://www.codefellows.org/"
+html = """
+<html>
+  <head></head>
+  <body>
+    <p>Hi!<br>
+        This is HTML<br>
+        How are you?<br>
+        I'm a scattered pea. <a href="https://www.codefellows.org/">Watch this</a>
+    </p>
+  </body>
+</html>
+ """
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+msg.attach(part1)
+msg.attach(part2)
 
 username = 'scatterpeas@gmail.com'
 password = 'peapassword'
