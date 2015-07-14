c95b0992c3bfe1ee6c6d8bc6cbb517c548e8d88b
Jason Tyler <jmtyler@gmail.com>
2015-07-14 15:17:51 -0700

    still implementing methods for Reminders, Jobs, and RRules; merging to get all of our work in a common place

--- models.py
+++ models.py
@@ -15,14 +15,14 @@ Base = declarative_base()
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
-DBSession1 = Session()
+DBSession = Session()
+
 
 class Reminders(Base):
 
     __tablename__ = 'reminders'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     alias_id = Column(Integer, ForeignKey('aliases.id'), nullable=False)
     # keeps the rrule currently generating jobs
     rrule_id = Column(Integer, ForeignKey('rrules.id'), nullable=False)
@@ -34,14 +34,43 @@ class Reminders(Base):
     # turned to False after execution of last job
     rstate = Column(Integer)
     # next_event = Column(DateTime(timezone=True))
+    child = relationship("", backref=backref("reminders", uselist=False))
+
+    @classmethod
+    def create(cls, title="", text_payload="", media_payload="",
+               rstate=True, session=None, user_id=0, alias_id=0):
+        if session is None:
+            session = DBSession
+        if title != "" and alias_id != 0:
+            '''Reminder instance is created, but may not potentially have
+            all necessary attributes including rrule_id. It is the parent 
+            function's responsibility to provide
+            these.
+            '''
+            instance = cls(title=title,
+                           text_payload=text_payload,
+                           media_payload=media_payload, rstate=rstate)
+            session.add(instance)
+            return instance
 
 
 class RRules(Base):
-    """Contains iCal specification rules corresponding to an event. """
+    """Contains iCal specification rules corresponding to an event. This
+    class is not yet fully implemented, but stands in for future
+    expandability"""
     __tablename__ = 'rrules'
     # constrain this to primary key of reminders
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     reminder_id = Column(Integer, ForeignKey('reminders.id'))
+
+    @classmethod
+    def create(cls, reminder_id=0, session=None):
+        if session is None:
+            session = DBSession
+        if reminder_id != 0:
+            instance = cls(reminder_id=reminder_id)
+            session.add(instance)
+            return instance
 
 
 class Jobs(Base):
