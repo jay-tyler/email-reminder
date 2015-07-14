dfa1029044e98ce10b1558391cfb677adc0dbc57
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-13 18:38:04 -0700

    change default values to unicode

--- ScatterPeas/scatterpeas/models.py
+++ ScatterPeas/scatterpeas/models.py
@@ -28,7 +28,7 @@ from zope.sqlalchemy import ZopeTransactionExtension
 
 DATABASE_URL = os.environ.get(
     'DATABASE_URL',
-    'postgresql:///scatterPeas1'
+    'postgresql:///scatterpeas1'
 )
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
@@ -42,41 +42,41 @@ class User(Base):
     last = Column(Unicode(50))
     username = Column(Unicode(50), nullable=False)
     password = Column(Unicode(60), nullable=False)
-    dflt_medium = Column(Unicode(5), default='email', nullable=False)
+    dflt_medium = Column(Unicode(5), default=u'email', nullable=False)
     timezone = Column(
         Unicode(50),
-        default='America/Los_Angeles',
+        default=u'America/Los_Angeles',
         nullable=False
     )
 
-    aliases = relationship('Alias', order_by='Alias.id', backref='users')
-    reminders = relationship(
-        'Reminder',
-        order_by='Reminder.id',
-        backref='users'
-    )
+    aliases = relationship('Alias', order_by='Alias.id', backref=backref('users', order_by=id))
+    # reminders = relationship(
+    #     'Reminder',
+    #     order_by='Reminder.id',
+    #     backref='users'
+    # )
 
     def __repr__(self):
-        return "<User(first=' %s', last=' %s', username=' %s', dflt_medium=' %s')>" % (self.first, self.last, self.username, self.dflt_medium)
+        return "<User(first='%s', last='%s', username='%s', dflt_medium='%s')>" % (self.first, self.last, self.username, self.dflt_medium)
 
 
 class Alias(Base):
     __tablename__ = 'aliases'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    alias = Column(Unicode(100), default='ME', nullable=False)
+    alias = Column(Unicode(100), default=u'ME', nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     contact = Column(Unicode(75))
     contact_state = Column(Integer, default=0)
 
-    uuids = relationship('UUID', order_by='UUID.id', backref='aliases')
-    reminders = relationship(
-        'Reminder',
-        order_by='Reminder.id',
-        backref='aliases'
-    )
+    uuids = relationship('UUID', order_by='UUID.id', backref=backref('aliases', order_by=id))
+    # reminders = relationship(
+    #     'Reminder',
+    #     order_by='Reminder.id',
+    #     backref='aliases'
+    # )
 
     def __repr__(self):
-        return "<Alias(name=' %s', contact=' %s', c_state=' %s', user_id=' %s')>" %  (self.alias, self.contact, self.contact_medium, self.user_id)
+        return "<Alias(name='%s', contact='%s', c_state='%s', user_id='%s')>" %  (self.alias, self.contact, self.contact, self.user_id)
 
 
 class UUID(Base):
@@ -84,17 +84,19 @@ class UUID(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     uuid = Column(Unicode(36), nullable=False, default=uuid.uuid4)
     alias_id = Column(Integer, ForeignKey('aliases.id'), nullable=False)
-    medium = Column(Unicode(5), default='email', nullable=False)
+    medium = Column(Unicode(5), default=u'email', nullable=False)
     created = Column(
         DateTime,
         default=datetime.datetime.utcnow,
         nullable=False)
 
     def __repr__(self):
-        return "<UUID(uuid=' %s', medium=' %s', created=' %s', alias_id=' %s')>" %  (self.uuid, self.medium, self.created, self.alias_id)
+        return "<UUID(uuid='%s', medium='%s', created='%s', alias_id='%s')>" %  (self.uuid, self.medium, self.created, self.alias_id)
 
 
-def init_db():
+if __name__ == '__main__':
+    foo = User(first='foo', last='bar', username='foobar', password='asdf')
+    biz = User(first='biz', last='baz', username='bizbaz', password='1qaz')
     engine = sa.create_engine(DATABASE_URL, echo=True)
     Base.metadata.create_all(engine)
     DBSession.configure(bind=engine)
