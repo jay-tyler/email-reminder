adcfd43bf9dd93838722a56cdfa0b7dc8502d9dc
Saki Fu <sakifu.86@gmail.com>
2015-07-14 14:49:32 -0700

    edited receiver

--- email_receiver_gmail.py
+++ email_receiver_gmail.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
+import email
 import imaplib
 
-username = 'scatterpeas'
-password = 'peapassword'
 
-mail = imaplib.IMAP4_SSL("imap.gmail.com")
+def receive():
+    username = ''
+    password = ''
 
-mail.login(username, password)
-mail.list()
-mail.select("inbox")
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(username, password)
+    mail.list()
+    mail.select("inbox")
+    result, data = mail.uid('search', None, "ALL")
+    latest_email_uid = data[0].split()[-1]
+    result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+    raw_email = data[0][1]
+    print raw_email
+    email_message = email.message_from_string(raw_email)
+    toaddrs = email_message['To'].decode('UTF-8')
+    fromaddr = email_message['From'].decode('UTF-8')
+    print toaddrs
+    print fromaddr
+    maintype = email_message.get_content_maintype()
+    if maintype == 'multipart':
+        for part in email_message.get_payload():
+            if part.get_content_maintype() == 'text':
+                body = part.get_payload().decode('UTF-8')
+                print body
+                return email_message.get_payload()
+    elif maintype == 'text':
+        body = part.get_payload().decode('UTF-8')
+        print body
+        return email_message.get_payload()
 
-result, data = mail.uid('search', None, "ALL")
-latest_email_uid = data[0].split()[-1]
-result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-raw_email = data[0][1]
-print raw_email
-mail.logout()
+
+if __name__ == '__main__':
+    receive()
