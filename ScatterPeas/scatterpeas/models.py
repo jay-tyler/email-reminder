a6d872d462364bf2f540f8fff32b59dfd89303d3
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-15 14:23:06 -0700

    full version of User, Alias, and UUID before tests

--- ScatterPeas/scatterpeas/models.py
+++ ScatterPeas/scatterpeas/models.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import os
 import uuid
-import datetime
+from datetime import datetime, timedelta
 from sqlalchemy import (
     Column,
     Index,
@@ -31,7 +32,7 @@ from zope.sqlalchemy import ZopeTransactionExtension
 
 DATABASE_URL = os.environ.get(
     'DATABASE_URL',
-    'postgresql:///scatterpeas2'
+    'postgresql:///scatterpeas3'
 )
 
 # DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
@@ -44,59 +45,59 @@ DBSession = Session()
 # Session = sessionmaker(bind=engine)
 
 
-# class Reminders(Base):
+class Reminders(Base):
 
-#     __tablename__ = 'reminders'
+    __tablename__ = 'reminders'
 
-#     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-#     alias_id = Column(Integer, ForeignKey('aliases.id'), nullable=False)
-#     # keeps the rrule currently generating jobs
-#     rrule_id = Column(Integer, ForeignKey('rrules.id'), nullable=False)
-#     title = Column(Unicode(256), nullable=False)
-#     text_payload = Column(Unicode(20000))
-#     # this might end up just being a link to a static resourc; we will see
-#     media_payload = Column(Unicode(256))
-#     # rstate is used to to track whether a reminder is still producing jobs
-#     # turned to False after execution of last job
-#     rstate = Column(Integer)
-#     # next_event = Column(DateTime(timezone=True))
-#     child = relationship("", backref=backref("reminders", uselist=False))
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    alias_id = Column(Integer, ForeignKey('aliases.id'), nullable=False)
+    # keeps the rrule currently generating jobs
+    rrule_id = Column(Integer, ForeignKey('rrules.id'), nullable=False)
+    title = Column(Unicode(256), nullable=False)
+    text_payload = Column(Unicode(20000))
+    # this might end up just being a link to a static resourc; we will see
+    media_payload = Column(Unicode(256))
+    # rstate is used to to track whether a reminder is still producing jobs
+    # turned to False after execution of last job
+    rstate = Column(Integer)
+    # next_event = Column(DateTime(timezone=True))
+    # child = relationship("", backref=backref("reminders", uselist=False))
 
-#     @classmethod
-#     def create(cls, title="", text_payload="", media_payload="",
-#                rstate=True, session=None, user_id=0, alias_id=0):
-#         if session is None:
-#             session = DBSession
-#         if title != "" and alias_id != 0:
-#             '''Reminder instance is created, but may not potentially have
-#             all necessary attributes including rrule_id. It is the parent
-#             function's responsibility to provide
-#             these.
-#             '''
-#             instance = cls(title=title,
-#                            text_payload=text_payload,
-#                            media_payload=media_payload, rstate=rstate)
-#             session.add(instance)
-#             return instance
+    @classmethod
+    def create(cls, title="", text_payload="", media_payload="",
+               rstate=True, session=None, user_id=0, alias_id=0):
+        if session is None:
+            session = DBSession
+        if title != "" and alias_id != 0:
+            '''Reminder instance is created, but may not potentially have
+            all necessary attributes including rrule_id. It is the parent
+            function's responsibility to provide
+            these.
+            '''
+            instance = cls(title=title,
+                           text_payload=text_payload,
+                           media_payload=media_payload, rstate=rstate)
+            session.add(instance)
+            return instance
 
 
-# class RRules(Base):
-#     """Contains iCal specification rules corresponding to an event. This
-#     class is not yet fully implemented, but stands in for future
-#     expandability"""
-#     __tablename__ = 'rrules'
-#     # constrain this to primary key of reminders
-#     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-#     reminder_id = Column(Integer, ForeignKey('reminders.id'))
+class RRules(Base):
+    """Contains iCal specification rules corresponding to an event. This
+    class is not yet fully implemented, but stands in for future
+    expandability"""
+    __tablename__ = 'rrules'
+    # constrain this to primary key of reminders
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    reminder_id = Column(Integer, ForeignKey('reminders.id'))
 
-#     @classmethod
-#     def create(cls, reminder_id=0, session=None):
-#         if session is None:
-#             session = DBSession
-#         if reminder_id != 0:
-#             instance = cls(reminder_id=reminder_id)
-#             session.add(instance)
-#             return instance
+    @classmethod
+    def create(cls, reminder_id=0, session=None):
+        if session is None:
+            session = DBSession
+        if reminder_id != 0:
+            instance = cls(reminder_id=reminder_id)
+            session.add(instance)
+            return instance
 
 
 class User(Base):
@@ -106,10 +107,11 @@ class User(Base):
     last = Column(Unicode(50))
     username = Column(Unicode(50), nullable=False)
     password = Column(String(60), nullable=False)
-    dflt_medium = Column(Unicode(5), default=u'email', nullable=False)
+    # medium state is 1 for email 2 for text
+    dflt_medium = Column(Integer, default=1, nullable=False)
     timezone = Column(
         Unicode(50),
-        default=u'America/Los_Angeles',
+        default='America/Los_Angeles',
         nullable=False
     )
 
@@ -125,8 +127,8 @@ class User(Base):
     # )
 
     @classmethod
-    def create_user(cls, username, password, first=None, last=None,
-                    dflt_medium=None, timezone=None, session=None):
+    def create_user(cls, username, password, first="", last="",
+                    dflt_medium=1, timezone='America/Los_Angeles', session=None):
         if session is None:
             session = DBSession
         manager = BCRYPTPasswordManager()
@@ -170,7 +172,7 @@ class Alias(Base):
     """
     __tablename__ = 'aliases'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    alias = Column(Unicode(100), default=u'ME', nullable=False)
+    alias = Column(Unicode(100), default='ME', nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     contact_info = Column(Unicode(75), nullable=False)
     medium = Column(Integer, default=1)
@@ -231,7 +233,7 @@ class UUID(Base):
     confirmation_state = Column(Integer, default=0, nullable=False)
     created = Column(
         DateTime,
-        default=datetime.datetime.utcnow,
+        default=datetime.utcnow(),
         nullable=False)
 
     @classmethod
@@ -262,7 +264,6 @@ class UUID(Base):
         """Queries for uuid row by uuid; returns a tuple containing
         (uuid.alias_id, uuid.confirmation_state)
         """
-        # this is not working! something about uuid return object
         if session is None:
             session = DBSession
         UUID._update_state(uuid)
@@ -289,8 +290,8 @@ class UUID(Base):
             session = DBSession
         uuid = UUID.by_uuid(uuid)
         if uuid.confirmation_state != 2:
-            eol = uuid.created + datetime.timedelta(days=1)
-            if eol < datetime.datetime.utcnow():
+            eol = uuid.created + timedelta(days=1)
+            if eol < datetime.utcnow():
                 uuid.confirmation_state = -1
 
     def __repr__(self):
@@ -304,16 +305,27 @@ def init_db():
     Base.metadata.create_all(engine)
 
 
+def helper():
+    biz = User.create_user(username='bizbaz', password='asdf')
+    DBSession.add(biz)
+    DBSession.commit()
+    bizmarkie = Alias.create_alias(biz.id, contact_info='biz@gmail.com')
+    DBSession.add(bizmarkie)
+    DBSession.commit()
+    buuid = UUID.create_uuid(bizmarkie.id)
+    DBSession.add(buuid)
+    DBSession.commit()
 
-# class Jobs(Base):
-#     __tablename__ = 'jobs'
-#     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-#     reminder_id = Column(Integer, ForeignKey('reminders.id'))
-#     # 0 for awaiting execution, 1 for executed successfully, 2 for failed
-#     # excecution, 3 for cancelled
-#     job_state = Column(Integer)
-#     # execution time in UTC
-#     execution_time = Column(DateTime)
+
+class Jobs(Base):
+    __tablename__ = 'jobs'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    reminder_id = Column(Integer, ForeignKey('reminders.id'))
+    # 0 for awaiting execution, 1 for executed successfully, 2 for failed
+    # excecution, 3 for cancelled
+    job_state = Column(Integer)
+    # execution time in UTC
+    execution_time = Column(DateTime)
 
 
 # if __name__ == '__main__':
