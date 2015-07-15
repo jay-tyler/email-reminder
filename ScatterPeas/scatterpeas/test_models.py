4451c0c4ef57b3fa8f5b6a8fff40bb26bfa66891
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-15 15:31:30 -0700

    test dummy function

---
+++ ScatterPeas/scatterpeas/test_models.py
@@ -0,0 +1,40 @@
+import os
+import pytest
+from sqlalchemy import create_engine
+from pyramid import testing
+
+import models
+
+TEST_DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    'postgresql:///test_scatterpeas'
+)
+
+os.environ['DATABASE_URL'] = TEST_DATABASE_URL
+
+
+@pytest.fixture(scope='session')
+def connection(request):
+    engine = create_engine(TEST_DATABASE_URL)
+    models.Base.metadata.create_all(engine)
+    connection = engine.connect()
+    models.DBSession.registry.clear()
+    models.DBSession.configure(bind=connection)
+    models.Base.metadata.bind = engine
+    request.addfinalizer(models.Base.metadata.drop_all)
+    return connection
+
+
+@pytest.fixture()
+def db_session(request, connection):
+    from transaction import abort
+    trans = connection.begin()
+    request.addfinalizer(trans.rollback)
+    request.addfinalizer(abort)
+
+    from models import DBSession
+    return DBSession
+
+def test_models(db_session):
+    pass
+    
