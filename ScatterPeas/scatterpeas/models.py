508a1ff645d15502e6db71a18922310f689875d9
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-15 17:00:22 -0700

    start tests for create_user

--- ScatterPeas/scatterpeas/models.py
+++ ScatterPeas/scatterpeas/models.py
@@ -35,69 +35,69 @@ DATABASE_URL = os.environ.get(
     'postgresql:///scatterpeas3'
 )
 
-# DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
-engine = create_engine(DATABASE_URL, echo=True)
-Session = sessionmaker(bind=engine)
-DBSession = Session()
+# engine = create_engine(DATABASE_URL, echo=True)
+# Session = sessionmaker(bind=engine)
+# DBSession = Session()
 # engine = create_engine(DATABASE_URL, echo=True)
 # Base.metadata.create_all(engine)
 # Session = sessionmaker(bind=engine)
 
 
-class Reminders(Base):
+# class Reminders(Base):
 
-    __tablename__ = 'reminders'
+#     __tablename__ = 'reminders'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    alias_id = Column(Integer, ForeignKey('aliases.id'), nullable=False)
-    # keeps the rrule currently generating jobs
-    rrule_id = Column(Integer, ForeignKey('rrules.id'), nullable=False)
-    title = Column(Unicode(256), nullable=False)
-    text_payload = Column(Unicode(20000))
-    # this might end up just being a link to a static resourc; we will see
-    media_payload = Column(Unicode(256))
-    # rstate is used to to track whether a reminder is still producing jobs
-    # turned to False after execution of last job
-    rstate = Column(Integer)
-    # next_event = Column(DateTime(timezone=True))
-    # child = relationship("", backref=backref("reminders", uselist=False))
+#     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+#     alias_id = Column(Integer, ForeignKey('aliases.id'), nullable=False)
+#     # keeps the rrule currently generating jobs
+#     rrule_id = Column(Integer, ForeignKey('rrules.id'), nullable=False)
+#     title = Column(Unicode(256), nullable=False)
+#     text_payload = Column(Unicode(20000))
+#     # this might end up just being a link to a static resourc; we will see
+#     media_payload = Column(Unicode(256))
+#     # rstate is used to to track whether a reminder is still producing jobs
+#     # turned to False after execution of last job
+#     rstate = Column(Integer)
+#     # next_event = Column(DateTime(timezone=True))
+#     # child = relationship("", backref=backref("reminders", uselist=False))
 
-    @classmethod
-    def create(cls, title="", text_payload="", media_payload="",
-               rstate=True, session=None, user_id=0, alias_id=0):
-        if session is None:
-            session = DBSession
-        if title != "" and alias_id != 0:
-            '''Reminder instance is created, but may not potentially have
-            all necessary attributes including rrule_id. It is the parent
-            function's responsibility to provide
-            these.
-            '''
-            instance = cls(title=title,
-                           text_payload=text_payload,
-                           media_payload=media_payload, rstate=rstate)
-            session.add(instance)
-            return instance
+#     @classmethod
+#     def create(cls, title="", text_payload="", media_payload="",
+#                rstate=True, session=None, user_id=0, alias_id=0):
+#         if session is None:
+#             session = DBSession
+#         if title != "" and alias_id != 0:
+#             '''Reminder instance is created, but may not potentially have
+#             all necessary attributes including rrule_id. It is the parent
+#             function's responsibility to provide
+#             these.
+#             '''
+#             instance = cls(title=title,
+#                            text_payload=text_payload,
+#                            media_payload=media_payload, rstate=rstate)
+#             session.add(instance)
+#             return instance
 
 
-class RRules(Base):
-    """Contains iCal specification rules corresponding to an event. This
-    class is not yet fully implemented, but stands in for future
-    expandability"""
-    __tablename__ = 'rrules'
-    # constrain this to primary key of reminders
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    reminder_id = Column(Integer, ForeignKey('reminders.id'))
+# class RRules(Base):
+#     """Contains iCal specification rules corresponding to an event. This
+#     class is not yet fully implemented, but stands in for future
+#     expandability"""
+#     __tablename__ = 'rrules'
+#     # constrain this to primary key of reminders
+#     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+#     reminder_id = Column(Integer, ForeignKey('reminders.id'))
 
-    @classmethod
-    def create(cls, reminder_id=0, session=None):
-        if session is None:
-            session = DBSession
-        if reminder_id != 0:
-            instance = cls(reminder_id=reminder_id)
-            session.add(instance)
-            return instance
+#     @classmethod
+#     def create(cls, reminder_id=0, session=None):
+#         if session is None:
+#             session = DBSession
+#         if reminder_id != 0:
+#             instance = cls(reminder_id=reminder_id)
+#             session.add(instance)
+#             return instance
 
 
 class User(Base):
@@ -317,21 +317,13 @@ def helper():
     DBSession.commit()
 
 
-class Jobs(Base):
-    __tablename__ = 'jobs'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    reminder_id = Column(Integer, ForeignKey('reminders.id'))
-    # 0 for awaiting execution, 1 for executed successfully, 2 for failed
-    # excecution, 3 for cancelled
-    job_state = Column(Integer)
-    # execution time in UTC
-    execution_time = Column(DateTime)
+# class Jobs(Base):
+#     __tablename__ = 'jobs'
+#     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+#     reminder_id = Column(Integer, ForeignKey('reminders.id'))
+#     # 0 for awaiting execution, 1 for executed successfully, 2 for failed
+#     # excecution, 3 for cancelled
+#     job_state = Column(Integer)
+#     # execution time in UTC
+#     execution_time = Column(DateTime)
 
-
-# if __name__ == '__main__':
-#     # foo = User.create_user(first='foo', last='bar', username='foobar', password='asdf')
-#     biz = m.User.create_user(first='biz', last='baz', username='bizbaz', password='1qaz')
-#     engine = create_engine(DATABASE_URL, echo=True)
-#     Base.metadata.create_all(engine)
-#     Session = sessionmaker(bind=engine)
-#     Session.configure(bind=engine)
--- ScatterPeas/scatterpeas/tests/test_models.py
+++ ScatterPeas/scatterpeas/tests/test_models.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import os
 import pytest
 from sqlalchemy import create_engine
 from pyramid import testing
+from cryptacular.bcrypt import BCRYPTPasswordManager
 
-import models
+from scatterpeas import models
 
 TEST_DATABASE_URL = os.environ.get(
     'DATABASE_URL',
@@ -21,7 +24,7 @@ def connection(request):
     models.DBSession.registry.clear()
     models.DBSession.configure(bind=connection)
     models.Base.metadata.bind = engine
-    request.addfinalizer(models.Base.metadata.drop_all)
+    # request.addfinalizer(models.Base.metadata.drop_all)
     return connection
 
 
@@ -32,9 +35,29 @@ def db_session(request, connection):
     request.addfinalizer(trans.rollback)
     request.addfinalizer(abort)
 
-    from models import DBSession
-    return DBSession
+    return models.DBSession
 
-def test_models(db_session):
-    pass
-    
+
+def test_create_user(db_session):
+    kwargs = {'first': 'foo', 'last': 'bar', 'username': 'foob',
+              'password': 'asdf'}
+    kwargs['session'] = db_session
+    assert db_session.query(models.User).count() == 0
+
+    user = models.User.create_user(**kwargs)
+    assert isinstance(user, models.User)
+
+    assert getattr(user, 'id') is None
+    assert getattr(user, 'dflt_medium') == 1
+    assert getattr(user, 'timezone') == 'America/Los_Angeles'
+
+    db_session.flush()
+    assert db_session.query(models.User).count() == 1
+    for field in kwargs:
+        if field != 'session' and field != 'password':
+            assert getattr(user, field) == kwargs[field]
+
+    manager = BCRYPTPasswordManager()
+    hashed = manager.encode()
+    assert getattr(user, 'password') == hashed
+
