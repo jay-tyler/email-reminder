710002d2ae841d8a6001f10e47e9c1a43b32f24e
Saki Fu <sakifu.86@gmail.com>
2015-07-16 12:30:50 -0700

    add sms

---
+++ send_sms.py
@@ -0,0 +1,25 @@
+import twilio
+from twilio.rest import TwilioRestClient
+
+account_sid = ""
+auth_token = ""
+
+
+def send_sms(body, tonum, fromnum, media_url=None):
+    try:
+        client = TwilioRestClient(account_sid, auth_token)
+        message = client.messages.create(body='{}'.format(body), to='{}'.format(tonum), from_='{}'.format(fromnum), media_url='{}'.format(media_url))
+        print message.sid
+    except twilio.TwilioRestException as e:
+        print e
+
+
+def receive_sms():
+    client = TwilioRestClient(account_sid, auth_token)
+    for message in client.messages.list():
+        print message.body
+
+
+if __name__ == '__main__':
+    send_sms('hellopea', '+17863009899', '+16319564194', 'https://theinfinitevariety.files.wordpress.com/2010/11/47nakedmolerat.jpg')
+    # receive_sms()
