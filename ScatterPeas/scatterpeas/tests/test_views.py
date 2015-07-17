78abc87df8f084f8cd81cb44ca19ca66a5e896b2
Nicholas Draper <ndraper2@gmail.com>
2015-07-17 00:45:40 -0700

    starting tests for the view functions

---
+++ ScatterPeas/scatterpeas/tests/test_views.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import pytest
+import os
+from sqlalchemy import create_engine
+from scatterpeas import models
+from scatterpeas.models import User, Alias, Reminder, RRule, Job
+
+TEST_DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    'postgresql:///test_scatterpeas'
+)
+
+os.environ['DATABASE_URL'] = TEST_DATABASE_URL
+
+
+@pytest.fixture(scope='session')
+def connection(request):
+    engine = create_engine(TEST_DATABASE_URL)
+    models.Base.metadata.create_all(engine)
+    connection = engine.connect()
+    models.DBSession.registry.clear()
+    models.DBSession.configure(bind=connection)
+    models.Base.metadata.bind = engine
+    request.addfinalizer(models.Base.metadata.drop_all)
+    return connection
+
+
+@pytest.fixture()
+def db_session(request, connection):
+    from transaction import abort
+    trans = connection.begin()
+    request.addfinalizer(trans.rollback)
+    request.addfinalizer(abort)
+
+    return models.DBSession
